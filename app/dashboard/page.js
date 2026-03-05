09f6205281bb9d08a8b304ab5d41b32531107c01
DixitSTTL <dixit.kukadiya@silvertouch.com>
2026-03-05 17:38:43 +0530

    issue fix

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -27,7 +27,7 @@ export default async function Dashboard() {
 
 async function fetchProducts() {
     try {
-        const res = await fetch('http://localhost:3000/api/products', { cache: 'no-store' });
+        const res = await fetch('/apis/products', { cache: 'no-store' });
         if (!res.ok) throw new Error('Failed to fetch');
         return await res.json();
     } catch (error) {
